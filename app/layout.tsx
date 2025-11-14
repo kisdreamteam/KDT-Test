3e8ca03b53247ab28132d582502e5e1c55f27d7b
kisdreamteam <kisdreamteam@gmail.com>
2025-11-14 19:37:03 +0700

    feat: updat scale to 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   width: 'device-width',
-  initialScale: 2,
+  initialScale: 1,
 }
 
 export default function RootLayout({
