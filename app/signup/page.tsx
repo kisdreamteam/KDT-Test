670d6b673c3d9b34b62ed4c7bf3f9178f44b7fca
kisdreamteam <kisdreamteam@gmail.com>
2025-11-16 20:29:04 +0700

    feat vercel

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,5 @@
 import SignupApp from '@/components/auth/SignupApp';
 
 export default function SignupPage() {
-  return <SignupApp min-h-screen bg-[#4A3B8D] />;
+  return <SignupApp bg:red-500/>;
 }
